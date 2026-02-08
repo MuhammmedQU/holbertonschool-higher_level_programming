c42e50b5f7c95481bede98d19068f3d0974c26ca
MuhammedQU <ahmedov.muhammed1611@gmail.com>
2026-02-08 10:39:29 +0000

    vi 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programming\
   language that combines remarkable power with very clear syntax"
 print(str[39:67] + str[106:111] + str[111:115] + str[115:122] + str[122:128] + str[128:132])
 
-
