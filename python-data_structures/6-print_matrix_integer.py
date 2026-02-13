3917de02012db0590ed3e03ca91d96ce2dd2bc55
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-13 12:28:55 +0000

    print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if  matrix == [[]] or len(matrix) == 0 or len(matrix[0]) == 0:
+    if matrix == [[]] or len(matrix) == 0 or len(matrix[0]) == 0:
         print()
         return
     for row in matrix:
