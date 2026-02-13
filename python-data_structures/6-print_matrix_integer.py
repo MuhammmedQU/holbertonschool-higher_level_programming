3abd0631f0cb65d78faf011174851a010358451d
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-13 12:25:46 +0000

    print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if  matrix == [[]] or len(matrix) == 0 or len(matrix[0]) == 0:
+        print()
+        return
     for row in matrix:
         for num in range(len(row)):
             if num != len(row) - 1:
