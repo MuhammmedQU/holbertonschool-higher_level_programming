d8ff3ce5ade09e1a6fa20c9b82c51e0da62de77c
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-13 13:15:06 +0000

    print_7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    result=[]
+    for i in range(2):
+        a=0
+        b=0
+        if i<len(tuple_a):
+            a=tuple_a[i]
+        if i<len(tuple_b):
+            b=tuple_b[i]
+
+        result.append(a+b)
+    return tuple(result)
