5e5b1cf78c71f76eff4091dc1aca1579d3ee9b73
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-16 18:00:34 +0000

    print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if x < 0:
+        return 0
+    elif x > len(my_list):
+        x = len(my_list)
     for i in range(x):
         print(my_list[i], end=" ")
     print()
