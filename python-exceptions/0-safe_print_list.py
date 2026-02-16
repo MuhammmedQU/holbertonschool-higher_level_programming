8082b678fce26e17ef95fddbe187bdba73b3532d
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-16 18:29:02 +0000

    safeprint

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             count += 1
-        except IndexError:
+        except:
             break
 
     print()
