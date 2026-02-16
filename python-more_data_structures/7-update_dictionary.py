c65c4b45c164a62ce52cf446ad9335dec389f564
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-16 01:19:24 +0000

    newdicti

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print(f"{key}: {value}")
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
