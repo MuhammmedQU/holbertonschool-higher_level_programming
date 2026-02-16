b8de0068586909207d61da44592c520ae658f78b
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-16 18:17:00 +0000

    print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x < 0:
-        return 0
-    elif x > len(my_list):
-        x = len(my_list)
-    for i in range(x):
-        print(my_list[i], end="")
+    cout = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            cout += 1
+    except IndexError:
+        pass
     print()
-    return x
+    return cout
