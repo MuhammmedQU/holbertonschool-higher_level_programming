498070a3be748eceda03dcbd6efe509d305afe71
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-18 10:28:10 +0000

    addpint

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module for integer addition"""
 
-
 def add_integer(a, b=98):
     """Adds two integers and returns their sum"""
 
@@ -10,4 +9,14 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    if isinstance(a, float):
+        if a != a or a == float("inf") or a == float("-inf"):
+            raise TypeError("a must be an integer")
+        a = int(a)
+
+    if isinstance(b, float):
+        if b != b or b == float("inf") or b == float("-inf"):
+            raise TypeError("b must be an integer")
+        b = int(b)
+
+    return a + b
