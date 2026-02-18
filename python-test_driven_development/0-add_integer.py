3fc43385b9caeaf4e17c5f78dd03e1b4c98a1e54
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-18 09:59:16 +0000

    printaddfunc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for integer addition """
 def add_integer(a, b=98):
+    """add two integers and return the result"""
     if not isinstance(a,(int,float)):
         raise TypeError("a must be an integer")
     if not isinstance(b,(int,float)):
