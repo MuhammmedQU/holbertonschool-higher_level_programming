e8f8e840e3f2ebf0f0baf26b2a014ca6189ae0bf
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-21 10:12:31 +0000

    printtext

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """prints a text with 2 new lines after ., ? and :"""
 
+
 def text_indentation(text):
-    """print formated text"""
+    """print formatted text"""
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new_line = True  
+    new_line = True
 
     for i in range(len(text)):
-
-       
         if new_line and text[i] == " ":
             continue
-
         print(text[i], end="")
-
-        if text[i] in ".?:":   
+        if text[i] in ".?:":
             print("\n")
             new_line = True
         else:
