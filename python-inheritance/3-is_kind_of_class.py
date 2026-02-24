daca6c1d2ae00ae176c2dc018a620dbf85a2dfd0
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-24 19:31:00 +0000

    kindofclass

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,5 +2,5 @@
 """Class that inherits from another class"""
 
 def is_kind_of_class(obj, a_class):
-    """Returns True if the object is exactly an instance of the specified class ; otherwise False."""
+    """Returns True if the object  ; otherwise False."""
     return  isinstance(obj, a_class)
