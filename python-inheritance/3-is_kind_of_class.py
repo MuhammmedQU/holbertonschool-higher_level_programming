ea367207673781fa47fa58bbfb53ec46f398b144
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-24 19:32:03 +0000

    kindofclass

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class that inherits from another class"""
 
+
 def is_kind_of_class(obj, a_class):
     """Returns True if the object  ; otherwise False."""
-    return  isinstance(obj, a_class)
+    return isinstance(obj, a_class)
