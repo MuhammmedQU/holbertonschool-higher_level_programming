256e468d520aeec171b10c1be365fd09398a3bc2
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-26 05:49:31 +0000

    inhertis

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,15 +2,6 @@
 """Module that contains the function inherits_from"""
 
 
-
 def inherits_from(obj, a_class):
-    """Returns True if the object is an instance of a class."""
+    """Returns True if the object inherited from a_class."""
     return issubclass(type(obj), a_class) and type(obj) != a_class
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
