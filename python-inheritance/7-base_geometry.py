134783ebc79c870a2fa0d55b6c703afd24b883e4
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-27 08:39:43 +0000

    geoTXT

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Generate a class"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry(object):
-    """BaseGeometry class"""
+    """Represent base geometry."""
 
     def area(self):
-        """area method"""
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-
+        """Validate value."""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
