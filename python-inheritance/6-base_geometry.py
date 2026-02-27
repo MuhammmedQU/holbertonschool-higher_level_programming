2836b796008d32455a22ec2d92cfa24cc62e907d
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-27 08:18:53 +0000

    geometre

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Gemerate a class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented") 
