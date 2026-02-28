59f7158c532a517f82fb95d7553d090a06613853
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-02-28 13:45:48 +0000

    duck

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -9,12 +9,10 @@ class Shape(ABC):
 
     @abstractmethod
     def area(self):
-        """Calculate area"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Calculate perimeter"""
         pass
 
 
@@ -25,11 +23,9 @@ class Circle(Shape):
         self.__radius = radius
 
     def area(self):
-        """Return area of circle"""
         return 3.141592653589793 * (self.__radius ** 2)
 
     def perimeter(self):
-        """Return perimeter of circle"""
         return 2 * 3.141592653589793 * self.__radius
 
 
@@ -41,15 +37,12 @@ class Rectangle(Shape):
         self.__height = height
 
     def area(self):
-        """Return area of rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return perimeter of rectangle"""
         return 2 * (self.__width + self.__height)
 
 
 def shape_info(shape):
-    """Print shape info using duck typing"""
     print("Area: {}".format(shape.area()))
     print("Perimeter: {}".format(shape.perimeter()))
