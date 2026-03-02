53b78495c0afc6bd0a1ab95b6a4a76b0c1169064
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-02 09:46:44 +0000

    jsonString

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns the string representation of an json."""
+    return json.loads(my_str)
