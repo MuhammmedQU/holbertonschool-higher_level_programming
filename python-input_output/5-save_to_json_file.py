5867478a79ca6880855c2286d594f2fe20dd8540
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-02 09:54:49 +0000

    writejson

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """Module change strind and write file"""
 
 import json
 
 def save_to_json_file(my_obj, filename):
     """Returns the change of the string and write file"""
-    data=json.dumps(my_obj)
-
     with open(filename,"w") as f:
-        f.write(data)
+        json.dump(my_obj, f)
