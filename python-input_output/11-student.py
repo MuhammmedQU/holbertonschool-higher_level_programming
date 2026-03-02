4e360fa90bd64aef9865f23a0cc0bb22edf22a4d
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-02 11:11:30 +0000

    stundenrclassjson

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -8,7 +8,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self,attrs=None):
         if attrs is not None:
             result = {}
             for key, value in self.__dict__.items():
@@ -17,7 +17,6 @@ class Student:
             return result
         else:
             return self.__dict__
-        
     def reload_from_json(self, json):
         for key, value in json.items():
-            setattr(self, key, value)   
+         setattr(self, key, value)   
