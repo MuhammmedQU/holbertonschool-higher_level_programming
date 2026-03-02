445bca858a83cc99d4c814cb33cc9515dc49102e
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-02 11:09:33 +0000

    stundenrclassjson

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This file is the complete algorithm."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self,attrs=None):
+        if attrs is not None:
+            result = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    result[key] = value
+            return result
+        else:
+            return self.__dict__
+        
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)   
