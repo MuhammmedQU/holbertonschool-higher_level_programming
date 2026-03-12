fcc1df5006e1356d71f7aa55c637527c8a7337c5
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-12 13:14:45 +0000

    MyGenres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,6 @@
--- Write a SQL query
 SELECT tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-OERDER BY tv_genres.name ASC;
+ORDER BY tv_genres.name ASC;
