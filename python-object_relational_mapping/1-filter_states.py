2359994bb7c211ef64a1883ba0a53925ecf9e72e
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 09:42:53 +0000

    selecet

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""Lists all states starting with N from the database hbtn_0e_0_usa."""
+"""module to lists all states from the database"""
+
 
 import MySQLdb
 import sys
 
 
-def main():
-    """Connects to MySQL database and prints states starting with N."""
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    cursor = db.cursor()
-
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    )
-
+if __name__ == "__main__":
+    bdd = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3])
+    cursor = bdd.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' LIMIT 2")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-
     cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
+    bdd.close()
