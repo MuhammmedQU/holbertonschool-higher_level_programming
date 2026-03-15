160c0c45da587b9b2f7ec866f0830b5fcbf31d32
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 19:46:36 +0000

    citi

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""Module for Selecting cities"""
+"""Module for selecting cities"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=argv[1],
-        password=argv[2],
-        database=argv[3]
+        passwd=argv[2],
+        db=argv[3]
     )
+
     cursor = db.cursor()
 
-    cursor.execute('SELECT c.id, c.name, s.name \
-                    FROM cities AS c \
-                    INNER JOIN states AS s ON s.id = c.state_id ')
+    cursor.execute("""SELECT cities.id, cities.name, states.name
+                      FROM cities
+                      JOIN states ON cities.state_id = states.id
+                      ORDER BY cities.id ASC""")
 
-    for state in cursor.fetchall():
-        print(state)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
-
+    cursor.close()
+    db.close()
