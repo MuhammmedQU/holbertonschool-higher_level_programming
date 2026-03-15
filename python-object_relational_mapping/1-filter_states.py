f036bfbd0f4c0d27f3a19092b203d153d7945c63
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 19:37:53 +0000

    filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N from hbtn_0e_0_usa.
+'hbtn_0e_0_usa' bazasından adı 'N' ilə başlayan ştatları siyahılayan skript.
 """
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-
+    # Verilənlər bazasına qoşulma
+    # sys.argv[1] -> username, sys.argv[2] -> password, sys.argv[3] -> database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
+
     cursor = db.cursor()
-    # BINARY istifadə edirik ki, 'n' (kiçik) ilə başlamasın, ancaq 'N' olsun.
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-ORDER BY states.id ASC")
+
+    # SQL sorğusu: 'N' ilə başlayanları seçir və ID-yə görə sıralayır.
+    # BINARY açar sözü böyük/kiçik hərf fərqini (N vs n) dəqiqləşdirir.
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Bütün uyğun sətirləri götürürük
     rows = cursor.fetchall()
+
+    # Nəticələri çap edirik
     for row in rows:
         print(row)
+
+    # Bağlantıları bağlayırıq
     cursor.close()
     db.close()
