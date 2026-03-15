f7bc26c1ef657a837e9e6d651f1f8825a0f91fb5
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 19:45:23 +0000

    citi

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,27 +1,26 @@
-
 #!/usr/bin/python3
-"""
-Lists all cities from the database hbtn_0e_4_usa.
-"""
-import MySQLdb
-import sys
+"""Module for Selecting cities"""
 
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
-if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
-    cur = db.cursor()
-    # JOIN istifadə edərək şəhərləri ştat adları ilə birlikdə gətiririk
-    cur.execute("SELECT cities.id, cities.name, states.name \
-FROM cities JOIN states ON cities.state_id = states.id \
-ORDER BY cities.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    cursor = db.cursor()
+
+    cursor.execute('SELECT c.id, c.name, s.name \
+                    FROM cities AS c \
+                    INNER JOIN states AS s ON s.id = c.state_id ')
+
+    for state in cursor.fetchall():
+        print(state)
+
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
+
