3e89030e50dd03d6d9a669ec4358f1f661d00e03
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 09:19:36 +0000

    filter

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,74 +1,27 @@
 #!/usr/bin/python3
-# """Simple script to read states from MySQL database"""
+# Lists all states from the database hbtn_0e_0_usa
 
 import MySQLdb
 import sys
 
 
-def main():
-    """Connect to MySQL and fetch states"""
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == "__main__":
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    for row in cursor.fetchall():
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
 
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
-
-
-##########` task 2`
-
-!/usr/bin/python3
-"""Simple script to read states from MySQL database"""
-
-import MySQLdb
-import sys
-
-
-def main():
-    """Connect to MySQL and fetch states"""
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    for row in cursor.fetchall():
-        print(row)
-
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
