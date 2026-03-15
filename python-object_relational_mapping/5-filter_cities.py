79b3967a53335bee944e7b1956a98401265107b3
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 09:16:50 +0000

    filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 # Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
     c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+
+    c.execute("""SELECT cities.name
+                 FROM cities
+                 JOIN states ON cities.state_id = states.id
+                 WHERE states.name = %s
+                 ORDER BY cities.id ASC""", (sys.argv[4],))
+
+    rows = c.fetchall()
+    print(", ".join([row[0] for row in rows]))
