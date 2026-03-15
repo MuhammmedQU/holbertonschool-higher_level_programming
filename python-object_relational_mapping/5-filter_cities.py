2666cb9ca14b5ff4aa9cb24e371be92738b05732
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-15 09:00:15 +0000

    filter

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Simple script to read states from MySQL database"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Connect to MySQL and fetch states"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    statename = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC", (statename,))
+
+    for row in cursor.fetchall():
+        print(row, end=",")
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main() 
