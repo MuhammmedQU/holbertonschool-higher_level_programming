02e676e40bd5db328de9a51b24cf413d4d31821f
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-26 16:10:34 +0400

    java7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2]);
+
+if (isNaN(a)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (a-- > 0) {
+    console.log('C is fun');
+  }
+}
