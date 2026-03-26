a76ce77feedd522751219b02b86c51d676f4f811
MuhammmedQU <ahmedov.muhammed1611@gmail.com>
2026-03-26 16:27:20 +0400

    java9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b){
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 
-
-a = parseInt(process.argv[2]);
-b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
 console.log(add(a, b));
